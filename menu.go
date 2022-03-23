070eac38bd70f700c97ddf0dba402ab0bd218d3d
游润之 <yourunzhi@yourunzhideMacBook-Air.local>
2022-03-23 10:36:03 +0800

    add main.go

--- menu.go
+++ menu.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func main() {
+	var command string
+
+	fmt.Println("type \"hello\"、\"help\"、\"quit\" for more")
+
+	for {
+		fmt.Scanf("%s", &command)
+		switch command {
+		case "hello":
+			fmt.Println("\thi this is your demo :-)")
+		case "help":
+			fmt.Println("\there's help menu")
+		case "quit":
+			return
+		default:
+			fmt.Println("\twrong command!")
+		}
+	}
+}
